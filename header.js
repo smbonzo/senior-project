b95e4d252b1995fbea1e0126ea529e0c2afc70d0
bml2238 <bml2238@rit.edu>
2023-09-21 15:30:58 -0400

    added header with jquery

---
+++ header.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {  
+    $('#header').append(
+        '<h1>' +
+            'Partially Hydrated devs' +
+        '</h1>' +
+        '<h2>' +
+            'SUNY Oswego - Ph.D. Candidate Search Engine and Database' +
+        '</h2>' +
+
+        '<nav class="links">' +
+            '<ul>' +
+                '<li><a href="index.html">Home</a></li>' +
+                '<li><a href="weekly4ups.html">Weekly 4-Ups</a></li>' +
+                '<li><a href="agenda.html">Agenda</a></li>' +
+                '<li><a href="timesheet.html">Timesheet</a></li>' +
+            '</ul>' +
+        '</nav>'
+    );
+});
