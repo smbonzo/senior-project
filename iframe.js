5c48539782f185ee34230f00dd4f676111f10c9f
James Logan <james@dbej.net>
2023-10-25 19:51:51 -0400

    updated iframe code to actually work and added missing agendas

--- iframe.js
+++ iframe.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-    const src = $(".iframe").attr("src")
-    $(".iframe").append(
-        `<div class="iframe-content">
-            <iframe 
-                id="embedded-document"
-                src="${src}"
-                height="100%"
-                width="100%"
-                frameborder="0"
-            ></iframe>
-        </div>`
-    );
+    console.log($(".iframe"));
+    const frames = $(".iframe");
+    for (let i = 0; i < frames.length; i++) {
+        const jIframe = $(frames[i]);
+        const src = jIframe.attr("src")
+        jIframe.append(
+            `<div class="iframe-content">
+                <iframe 
+                    id="embedded-document"
+                    src="${src}"
+                    height="100%"
+                    width="100%"
+                    frameborder="0"
+                ></iframe>
+            </div>`
+        );
+    }
+   
 });
